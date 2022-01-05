0e715e2eb73ce0c84396fb60d660e860381b91ca
eksim <eksim@DESKTOP-HNJ3OVT>
2022-01-05 16:06:24 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği 
+            //Do not repeat yourself - kendini tekrarlama 
+            //değer tutucu, alias 
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmismi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Oku");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu ");
+            }
+
+
+            if (sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
