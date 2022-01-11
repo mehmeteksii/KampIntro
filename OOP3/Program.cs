caee2aed54d95390e7e24c09e9362da367ad9528
mehmeteksii <eksimehmet02@gmail.com>
2022-01-11 19:53:04 +0300

    Ders Sonu 2

---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtitacKrediManager();//referans verildi
+            IKrediManager tasitKrediManager = new TasitKrediManager();//referans verildi
+            IKrediManager konutKrediManager = new KonutKrediManager();//referans verildi
+
+            ILoggerService databaeLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();//buraya database logger ide referans olarak verebilirdik
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager, databaeLoggerService); 
+            //basvuruManager.BasvuruYap(konutKrediManager,new DataBaseLoggerService());       //referansı bu şekilde de verebilirsin 
+
+            List<IKrediManager> krediler =  new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager};
+           
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop2
+{
+    //is sınıfları // business Classes
+     class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
